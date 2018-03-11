35548af404adab7b6e99c3ee0f4285c90bae2511
s.matthew.english <s.matthew.english@gmail.com>
2018-03-11 03:05:41 -0400

    concurrent access first attempt

--- TestNonBlockingIO.java
+++ TestNonBlockingIO.java
@@ -1,6 +1,7 @@
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -20,7 +21,7 @@ public class TestNonBlockingIO {
     Thread t = new Thread(new Runnable() {
       public void run() {
         try {
-          BlockingIO.run();
+          NonBlockingIO.run();
         } catch (Exception e) {
           e.printStackTrace();
         }
@@ -30,21 +31,73 @@ public class TestNonBlockingIO {
     t.start();
   }
 
+//  @Test
+//  public void testIO() throws Exception {
+//
+//    Socket socket = new Socket("127.0.0.1",1888);
+//    InputStream is = socket.getInputStream();
+//    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+//    int nRead;
+//    byte[] data = new byte[1024];
+//    while ((nRead = is.read(data, 0, data.length)) != -1) {
+//      buffer.write(data, 0, nRead);
+//    }
+//    buffer.flush();
+//
+//    String output = new String(buffer.toByteArray());
+//    System.out.println(output);
+//    socket.close();
+//  }
+
   @Test
-  public void testNonBlockingIO() throws Exception {
-
-    Socket socket = new Socket("127.0.0.1",1888);
-    InputStream is = socket.getInputStream();
-    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-    int nRead;
-    byte[] data = new byte[1024];
-    while ((nRead = is.read(data, 0, data.length)) != -1) {
-      buffer.write(data, 0, nRead);
+  public void testConcurrentIO() throws Exception {
+    // We will store the threads so that we can check if they are done
+    List<Thread> threads = new ArrayList<Thread>();
+    // We will create 500 threads
+    for (int i = 0; i < 100; i++) {
+      Runnable task = new MyRunnable();
+      Thread worker = new Thread(task);
+      // We can set the name of the thread
+      worker.setName(String.valueOf(i));
+      // Start the thread, never call method run() direct
+      worker.start();
+      // Remember the thread for later usage
+      threads.add(worker);
     }
-    buffer.flush();
+    int running = 0;
+    do {
+      running = 0;
+      for (Thread thread : threads) {
+        if (thread.isAlive()) {
+          running++;
+        }
+      }
+      System.out.println("We have " + running + " running threads. ");
+    } while (running > 0);
 
-    String output = new String(buffer.toByteArray());
-    System.out.println(output);
-    socket.close();
+  }
+}
+
+class MyRunnable implements Runnable {
+
+  @Override
+  public void run() {
+    try {
+      Socket socket = new Socket("127.0.0.1",1888);
+      InputStream is = socket.getInputStream();
+      ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+      int nRead;
+      byte[] data = new byte[1024];
+      while ((nRead = is.read(data, 0, data.length)) != -1) {
+        buffer.write(data, 0, nRead);
+      }
+      buffer.flush();
+
+      String output = new String(buffer.toByteArray());
+      System.out.println(output);
+      socket.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
